622b9fe09a79963a81a1c66cbd86a653c3117201
Luis Felipe Jimenez Ferreira <xLuis1749@gmail.com>
2022-03-15 21:30:20 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-var app = document.getElementById('location');
 
-
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-if (!isChrome){
-    $('#iframeAudio').remove()
-}
-else {
-    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-}
-
-var typewriter = new Typewriter(app, {
-  loop: false,
-  delay: 75,
-});
-
-
-typewriter.pauseFor(3000).typeString("St Jude Shrine <br>")
-typewriter.pauseFor(1000).typeString(`Saturday `)
-typewriter.pauseFor(1000).typeString(`05/21/2022`).start()
 
 
 
